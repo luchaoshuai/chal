256d832738ce806c0d622768fce313685f9d5b96
buke <wangbuke@gmail.com>
2018-12-07 12:11:41 +0000

    s1

---
+++ chal/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'PS Cloud 培训-2',
+    'version': '12.0.1.0',
+    'summary': 'PS Cloud 培训',
+    'author': "www.mypscloud.com",
+    'website': 'https://www.mypscloud.com/',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'js': [],
+    'css': [],
+    'auto_install': False,
+    'application': True,
+}
